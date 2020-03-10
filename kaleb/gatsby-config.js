da5ab50de37e713a99122ea163a270317f370b70
Caleb Thompson <caleb@webstronomers.com>
2020-03-10 07:20:57 -0700

    gatsby kaleb init

---
+++ kaleb/gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Kaleb in Gatsby`,
+    description: `Caleb the Webstronomer Gatsby Showcase App`,
+    author: `@caleb-the webstronomer`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#ffdc73`,
+        theme_color: `#ffdc73`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
